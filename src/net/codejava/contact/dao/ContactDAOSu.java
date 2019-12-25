16ae50c3c829bb8c22399bfa91275d6715608ede
ZIN MYO <overmidnight12@gmail.com>
2019-12-25 00:25:51 -0800

    my changes

---
+++ src/net/codejava/contact/dao/ContactDAOSu.java
@@ -0,0 +1,39 @@
+package net.codejava.contact.dao;
+
+import java.util.List;
+
+import net.codejava.contact.model.Contact;
+
+public class ContactDAOSu implements ContactDAO {
+
+	@Override
+	public int save(Contact contact) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Contact contact) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Contact get(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(Integer id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Contact> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
